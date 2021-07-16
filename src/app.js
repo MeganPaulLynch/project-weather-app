7f72c9570de29aa34588df8602c511c12c9b97b8
MeganPaulLynch <meganpaullynch@gmail.com>
2021-07-16 09:52:46 +0100

    Added API integration

--- src/app.js
+++ src/app.js
@@ -1,42 +1,20 @@
-function search(event) {
-  event.preventDefault();
-  let exploreCity = document.querySelector("#explore");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = exploreCity.value;
+function displayTemperature(response) {
+  console.log(response);
+  let tempC = document.querySelector("#weatherMeasure");
+  let cityElement = document.querySelector("h1");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#windElement");
+  let humidityElement = document.querySelector("#humidityElement");
+  tempC.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+}
+function searchCity(city) {
   let apiKey = "1f0ec59605e77b241bf0b174b889a407";
-  let city = exploreCity.value;
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=${units}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
   axios.get(apiUrl).then(displayTemperature);
 }
-let findCity = document.querySelector("#searchForm");
-findCity.addEventListener("submit", search);
-function displayTemperature(response) {
-  let temperature = Math.round(response.data.list[0].main.temp);
-  let tempC = document.querySelector("#weatherMeasure");
-  tempC.innerHTML = temperature;
-}
-function formatDate(date) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let currentDay = days[date.getDay()];
-  let currentHour = date.getHours();
-  if (currentHour < 10) {
-    currentHour = `0${currentHour}`;
-  }
-  let currentMinutes = date.getMinutes();
-  if (currentMinutes < 10) {
-    currentMinutes = `0${currentMinutes}`;
-  }
-  return `${currentDay} ${currentHour}:${currentMinutes}`;
-}
-let formattedDate = document.querySelector("#update");
-let date = new Date();
-formattedDate.innerHTML = formatDate(date);
+searchCity("Seoul");
